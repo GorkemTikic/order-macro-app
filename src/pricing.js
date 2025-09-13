06db3548ab1debe2a279785d6d097729ff0cfa48
GorkemTikic <grkmtkc94@gmail.com>
2025-09-13 06:37:07 +0300

    Update pricing.js

--- src/pricing.js
+++ src/pricing.js
@@ -27,7 +27,9 @@ function fmtUTC(ms) {
   const pad = (n) => String(n).padStart(2, "0");
   return `${d.getUTCFullYear()}-${pad(d.getUTCMonth() + 1)}-${pad(
     d.getUTCDate()
-  )} ${pad(d.getUTCMinutes())}:${pad(d.getUTCSeconds())} UTC+0`;
+  )} ${pad(d.getUTCHours())}:${pad(d.getUTCMinutes())}:${pad(
+    d.getUTCSeconds()
+  )} UTC+0`;
 }
 
 /**
