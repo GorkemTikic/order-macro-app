783de7a03df2105a8218bcbf34aa2b6cfbf50257
GorkemTikic <grkmtkc94@gmail.com>
2025-09-25 06:12:05 +0300

    Update index.js

--- src/macros/index.js
+++ src/macros/index.js
@@ -1,12 +1,14 @@
 // src/macros/index.js
 import { stopMarketMarkNotReached } from "./stop_market_mark_not_reached";
 import { stopMarketLossHigherThanExpectedMarkPrice } from "./stop_market_loss_higher_than_expected_mark_price";
+import { stopMarketLossHigherThanExpectedLastPrice } from "./stop_market_loss_higher_than_expected_last_price";
 import { fundingMacro } from "./funding_macro"; // normal import
 
 // Funding Macro intentionally excluded from MACROS list
 export const MACROS = [
   stopMarketMarkNotReached,
-  stopMarketLossHigherThanExpectedMarkPrice
+  stopMarketLossHigherThanExpectedMarkPrice,
+  stopMarketLossHigherThanExpectedLastPrice
 ];
 
 export const listMacros = () =>
